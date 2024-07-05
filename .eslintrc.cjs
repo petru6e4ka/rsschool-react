ba85d61c16a5015d95c35da383d6aba092b3aea2
anastasiiavorobiova <anastasiiavorobiova2404@gmail.com>
2024-07-05 21:36:58 +0300

    init: project setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		'airbnb',
+		'airbnb/hooks',
+		'airbnb-typescript',
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react-hooks/recommended',
+	],
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parser: '@typescript-eslint/parser',
+	plugins: ['react-refresh', 'react-compiler'],
+	rules: {
+		'react-refresh/only-export-components': [
+			'warn',
+			{ allowConstantExport: true },
+		],
+		'react/react-in-jsx-scope': 0,
+		'react-compiler/react-compiler': 'error',
+	},
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		project: ['./tsconfig.json', './tsconfig.app.json', './tsconfig.node.json'],
+		tsconfigRootDir: __dirname,
+		ecmaFeatures: {
+			jsx: true,
+		},
+	},
+};
