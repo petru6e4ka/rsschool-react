17b4091ee5693e6bcc891c98ff07ddd74542f1ed
anastasiiavorobiova <anastasiiavorobiova2404@gmail.com>
2024-07-14 21:32:07 +0300

    Updates

---
+++ assets/NotFound-CY1_uwCD.js
@@ -0,0 +1,1 @@
+import{c as s,j as o}from"./index-CwL0gbn2.js";const c="_NotFound_6fcmp_1";function e(){const n=s(),t=()=>{n(-1)};return o.jsxs("div",{className:c,children:[o.jsx("h1",{children:"The page is not found"}),o.jsx("p",{children:"Oops, you can go back"}),o.jsx("button",{type:"button",onClick:t,children:"Go back!"})]})}export{e as default};
